89d9de8d23b64bdd2b7949b0f84d3c141f20fe0c
YTRodi <yagorodi@gmail.com>
2022-03-07 23:54:54 -0300

    feat: test screens

---
+++ src/screens/PageOneScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const PageOneScreen = () => {
+  return (
+    <View>
+      <Text>PageOneScreen</Text>
+    </View>
+  );
+};
+
+export default PageOneScreen;
---
+++ src/screens/PageThreeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const PageThreeScreen = () => {
+  return (
+    <View>
+      <Text>PageThreeScreen</Text>
+    </View>
+  );
+};
+
+export default PageThreeScreen;
