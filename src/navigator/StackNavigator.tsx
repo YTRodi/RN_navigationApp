105c2c46176f0646dfa0140624f2c718c3cc26d7
YTRodi <yagorodi@gmail.com>
2022-03-07 23:55:01 -0300

    feat: StackNavigator

---
+++ src/navigator/StackNavigator.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import PageOneScreen from '../screens/PageOneScreen';
+import PageTwoScreen from '../screens/PageTwoScreen';
+import PageThreeScreen from '../screens/PageThreeScreen';
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="PageOneScreen" component={PageOneScreen} />
+    <Stack.Screen name="PageTwoScreen" component={PageTwoScreen} />
+    <Stack.Screen name="PageThreeScreen" component={PageThreeScreen} />
+  </Stack.Navigator>
+);
+
+export default StackNavigator;
