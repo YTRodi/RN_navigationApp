4eba867f69b8766dcd9d5bcdaa5dc833c8993b87
YTRodi <yagorodi@gmail.com>
2022-03-10 17:57:33 -0300

    feat: colors in appTheme

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,5 +1,9 @@
 import {StyleSheet} from 'react-native';
 
+export const colors = {
+  primary: '#5856D6',
+};
+
 export const styles = StyleSheet.create({
   globalMargin: {
     marginHorizontal: 20,
