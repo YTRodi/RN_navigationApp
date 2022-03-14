0c1e26cd4daf17918db5e4b2656be415cd4d1046
YTRodi <yagorodi@gmail.com>
2022-03-14 14:55:40 -0300

    feat: Icons in Drawer and PageOneScreen

--- src/screens/PageOneScreen.tsx
+++ src/screens/PageOneScreen.tsx
@@ -1,15 +1,47 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Button, Text, View, TouchableOpacity, ScrollView } from 'react-native';
 // import {StackScreenProps} from '@react-navigation/stack';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { RootStackParams } from '../navigator/StackNavigator';
-import { styles } from '../theme/appTheme';
+import { colors, styles } from '../theme/appTheme';
 
 // interface Props extends StackScreenProps<RootStackParams, 'PageOneScreen'> {}
 interface Props extends DrawerScreenProps<RootStackParams, 'PageOneScreen'> {}
 
 const PageOneScreen = ({ navigation }: Props) => {
+  const items = useMemo(
+    () => [
+      {
+        screen: 'PersonScreen',
+        params: { id: 1, nombre: 'Pedro' },
+        iconName: 'body-outline',
+        backgroundColor: '#5856D6',
+      },
+      {
+        screen: 'PersonScreen',
+        params: { id: 2, nombre: 'Maria' },
+        iconName: 'woman-outline',
+        backgroundColor: '#FF9427',
+      },
+    ],
+    [],
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <TouchableOpacity
+          style={{ marginLeft: 10 }}
+          onPress={() => navigation.toggleDrawer()}
+        >
+          <Icon name="menu-outline" size={35} color={colors.primary} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <ScrollView style={styles.globalMargin}>
       <Text style={styles.title}>PageOneScreen</Text>
@@ -24,29 +56,18 @@ const PageOneScreen = ({ navigation }: Props) => {
       </Text>
 
       <View style={{ flexDirection: 'row' }}>
-        <TouchableOpacity
-          style={{ ...styles.bigButton, backgroundColor: '#5856D6' }}
-          onPress={() =>
-            navigation.navigate('PersonScreen', {
-              id: 1,
-              nombre: 'Pedro',
-            })
-          }
-        >
-          <Text style={styles.bigButtonText}>Pedro</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{ ...styles.bigButton, backgroundColor: '#FF9427' }}
-          onPress={() =>
-            navigation.navigate('PersonScreen', {
-              id: 2,
-              nombre: 'Maria',
-            })
-          }
-        >
-          <Text style={styles.bigButtonText}>Maria</Text>
-        </TouchableOpacity>
+        {items.map(item => (
+          <TouchableOpacity
+            style={{
+              ...styles.bigButton,
+              backgroundColor: item.backgroundColor,
+            }}
+            onPress={() => navigation.navigate(item.screen as any, item.params)}
+          >
+            <Icon name={item.iconName} size={35} color="white" />
+            <Text style={styles.bigButtonText}>{item.params.nombre}</Text>
+          </TouchableOpacity>
+        ))}
       </View>
     </ScrollView>
   );
