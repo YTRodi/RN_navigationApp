35042df7df9ee8f5a6dde960860b2219c5ca5d64
YTRodi <yagorodi@gmail.com>
2022-03-14 14:35:23 -0300

    feat: Icons in TopTapNavigator

--- src/navigator/TopTapNavigator.tsx
+++ src/navigator/TopTapNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LogBox, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
@@ -26,33 +27,40 @@ export const TopTapNavigator = () => {
           elevation: 0, // remove shadows in android
           shadowColor: 'transparent', // remove shadows in ios
         },
-        tabBarShowIcon: true,
-        tabBarIcon: ({ color }) => {
-          const iconName = {
-            ChatScreen: 'CH',
-            ContactsScreen: 'CO',
-            AlbumsScreen: 'AL',
-          }[route.name];
+        tabBarLabel: ({ color, focused }) => {
+          const routeName = {
+            ChatScreen: 'Chat',
+            ContactsScreen: 'Contacts',
+            AlbumsScreen: 'Albums',
+          }[route.name]!;
 
-          return <Text style={{ color }}>{iconName}</Text>;
+          return (
+            <Text style={{ color: focused ? colors.primary : color }}>
+              {routeName}
+            </Text>
+          );
+        },
+        tabBarShowIcon: true,
+        tabBarIcon: ({ focused, color }) => {
+          const iconName = {
+            ChatScreen: 'chatbox-ellipses-outline',
+            ContactsScreen: 'people-outline',
+            AlbumsScreen: 'albums-outline',
+          }[route.name]!;
+
+          return (
+            <Icon
+              name={iconName}
+              size={20}
+              color={focused ? colors.primary : color}
+            />
+          );
         },
       })}
     >
-      <Tab.Screen
-        name="ChatScreen"
-        component={ChatScreen}
-        options={{ title: 'Chat' }}
-      />
-      <Tab.Screen
-        name="ContactsScreen"
-        component={ContactsScreen}
-        options={{ title: 'Contacts' }}
-      />
-      <Tab.Screen
-        name="AlbumsScreen"
-        component={AlbumsScreen}
-        options={{ title: 'Albums' }}
-      />
+      <Tab.Screen name="ChatScreen" component={ChatScreen} />
+      <Tab.Screen name="ContactsScreen" component={ContactsScreen} />
+      <Tab.Screen name="AlbumsScreen" component={AlbumsScreen} />
     </Tab.Navigator>
   );
 };
