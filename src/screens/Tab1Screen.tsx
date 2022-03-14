c067efa252345e9dbd2d37f14ef7871d14a348cd
YTRodi <yagorodi@gmail.com>
2022-03-14 11:36:54 -0300

    feat: Icons config in android

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { styles } from '../theme/appTheme';
 
 export const Tab1Screen = () => {
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Iconos</Text>
+
+      {/* https://ionic.io/ionicons */}
+      <Icon name="airplane-outline" size={80} color="#900" />
     </View>
   );
 };
