3384a53c379d4b8fffa753744ddf87192e0801aa
YTRodi <yagorodi@gmail.com>
2022-03-15 11:18:24 -0300

    feat: add authReducer

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+import { authReducer } from './authReducer';
 
 // Definir cómo luce, que información tendré aquí
 export interface AuthState {
@@ -25,11 +26,17 @@ export const AuthContext = createContext({} as AuthContextProps);
 
 // Componente proveedor del estado (High order component)
 export const AuthProvider = ({ children }: any) => {
+  const [authState, dispatch] = useReducer(authReducer, authInitialState);
+
+  const signIn = () => {
+    dispatch({ type: 'signIn' });
+  };
+
   return (
     <AuthContext.Provider
       value={{
-        authState: authInitialState,
-        signIn: () => {},
+        authState,
+        signIn,
       }}
     >
       {children}
--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { useContext } from 'react';
+import { Button, Text, View } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+  const { signIn, authState } = useContext(AuthContext);
+  const { isLoggedIn } = authState;
+
   return (
     <View>
       <Text>ContactsScreen</Text>
+
+      {!isLoggedIn && <Button title="SignIn" onPress={signIn} />}
     </View>
   );
 };
